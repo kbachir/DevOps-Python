e88f15001314e502514aa4a7c47939f886ab965f
kbachir <mrkbachir@spartaglobal.com>
2022-01-24 10:49:46 +0000

    Added variables intro

---
+++ 01_Variables/01_Variables_Intro.py
@@ -0,0 +1,17 @@
+#Variables [ctrl + / to make all lines comments]
+
+a = 1  #int (integer)
+b = 2
+c = 3.5  #float (floating_point_number)
+d = "Hello"  #str (string)
+
+print((a + c) * b)  #eg of calling a function. functionName(argument)
+print()
+
+result = (a + c) * b
+print(result, type(result))
+print(d, type(d))
+
+result = int((a + c) * b)
+print(result, type(result))
+
